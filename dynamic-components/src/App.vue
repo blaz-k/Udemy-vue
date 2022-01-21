c3c7f0057c08a7cc55c446d1f0b7b956c9b53a98
Blaz <blazyy@gmail.com>
2022-01-21 20:46:34 +0100

    dynamic component course

---
+++ dynamic-components/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <select v-model="componentName">
+      <option value="Home">Home</option>
+      <option value="About">About</option>
+    </select>
+    <keep-alive>
+      <component :is="componentName"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import Home from "./components/Home.vue";
+import About from "./components/About.vue";
+export default {
+  name: "App",
+  components: {
+    Home,
+    About,
+  },
+  data() {
+    return {
+      componentName: "Home",
+    };
+  },
+};
+</script>
+
+<style></style>
